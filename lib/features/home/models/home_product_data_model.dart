f5aff0099163b89b1070cf5b8f89e0583f16bc32
Aryan Bhasein <aryanbhasein1@gmail.com>
2024-08-07 22:57:50 +0530

    Product model added

---
+++ lib/features/home/models/home_product_data_model.dart
@@ -0,0 +1,9 @@
+class ProductDataModel {
+  final String name;
+  final String id;
+  final String description;
+  final String imageUrl;
+  final double price;
+
+  ProductDataModel({required this.name, required this.id, required this.description, required this.imageUrl, required this.price});
+}
