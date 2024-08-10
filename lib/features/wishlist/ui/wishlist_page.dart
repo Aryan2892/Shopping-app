b3a1250b5020ba476afff1e1d71cf68dce794f38
Aryan Bhasein <aryanbhasein1@gmail.com>
2024-08-10 17:13:19 +0530

    Wishlist page UI

--- lib/features/wishlist/ui/wishlist_page.dart
+++ lib/features/wishlist/ui/wishlist_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shopping_app/features/wishlist/bloc/wishlist_bloc.dart';
+import 'package:shopping_app/features/wishlist/ui/wishlist_tile_widget.dart';
 
 class WishlistPage extends StatefulWidget {
   const WishlistPage({super.key});
@@ -8,8 +11,40 @@ class WishlistPage extends StatefulWidget {
 }
 
 class _WishlistPageState extends State<WishlistPage> {
+  final WishlistBloc wishlistBloc = WishlistBloc();
+
+  @override
+  void initState() {
+    wishlistBloc.add(WishlistInitialEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Wishlist'),
+      ),
+      body: BlocConsumer<WishlistBloc, WishlistState>(
+        bloc: wishlistBloc,
+        listener: (context, state) {},
+        listenWhen: (previous, current) => current is WishlistActionState,
+        buildWhen: (previous, current) => current is !WishlistActionState,
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case const (WishlistSuccessState):
+              final successState = state as WishlistSuccessState;
+              return ListView.builder(
+                itemCount: successState.wishlistItems.length,
+                itemBuilder: (context, index) {
+                  return WishlistTileWidget(productDataModel: successState.wishlistItems[index], wishlistBloc: wishlistBloc);
+                },
+              );
+            default:
+          }
+          return Container();
+        },
+      ),
+    );
   }
 }
---
+++ lib/features/wishlist/ui/wishlist_tile_widget.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/features/cart/bloc/cart_bloc.dart';
+import 'package:shopping_app/features/home/models/home_product_data_model.dart';
+import 'package:shopping_app/features/wishlist/bloc/wishlist_bloc.dart';
+
+class WishlistTileWidget extends StatelessWidget {
+  final ProductDataModel productDataModel;
+  final WishlistBloc wishlistBloc;
+  const WishlistTileWidget(
+      {super.key, required this.productDataModel, required this.wishlistBloc});
+
+  @override
+  Widget build(BuildContext context) {
+    final cartBloc = CartBloc();
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.black),
+      ),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+          height: 200,
+          width: double.maxFinite,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              image: NetworkImage(productDataModel.imageUrl),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          productDataModel.name,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        Text(productDataModel.description),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "\$${productDataModel.price}",
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    wishlistBloc.add(WishlistRemoveFromWishlistEvent(
+                      productDataModel: productDataModel,
+                    ));
+                  },
+                  icon: const Icon(Icons.favorite),
+                ),
+                IconButton(
+                  onPressed: () {
+                    cartBloc.add(CartRemoveFromCartEvent(
+                      productDataModel: productDataModel,
+                    ));
+                  },
+                  icon: const Icon(Icons.shopping_bag),
+                ),
+              ],
+            )
+          ],
+        ),
+      ]),
+    );
+  }
+}
