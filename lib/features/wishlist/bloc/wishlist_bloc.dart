add470d06245c427feed40bedf8995b53c6712d0
Aryan Bhasein <aryanbhasein1@gmail.com>
2024-08-10 17:11:30 +0530

    Added wishlist bloc and its functionality

--- lib/features/wishlist/bloc/wishlist_bloc.dart
+++ lib/features/wishlist/bloc/wishlist_bloc.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping_app/data/wishlist_items.dart';
+import 'package:shopping_app/features/home/models/home_product_data_model.dart';
 // import 'package:meta/meta.dart';
 
 part 'wishlist_event.dart';
@@ -7,6 +11,16 @@ part 'wishlist_state.dart';
 
 class WishlistBloc extends Bloc<WishlistEvent, WishlistState> {
   WishlistBloc() : super(WishlistInitial()) {
-    on<WishlistEvent>((event, emit) {});
+    on<WishlistEvent>(wishlistInitialEvent);
+    on<WishlistRemoveFromWishlistEvent>(wishlistRemoveFromWishlistEvent);
+  }
+
+  FutureOr<void> wishlistInitialEvent(WishlistEvent event, Emitter<WishlistState> emit) {
+    emit(WishlistSuccessState(wishlistItems: wishlistItems));
+  }
+
+  FutureOr<void> wishlistRemoveFromWishlistEvent(WishlistRemoveFromWishlistEvent event, Emitter<WishlistState> emit) {
+    wishlistItems.remove(event.productDataModel);
+    emit(WishlistSuccessState(wishlistItems: wishlistItems));
   }
 }
